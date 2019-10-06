21a7c10ed9d42b12c98a24472ba3cf8beb7b528c
Budkovsky <piotr.budek@gmail.com>
2019-10-06 16:00:10 +0200

    ValidatorCollection change to indexed collection

--- src/Collection/ValidatorCollection.php
+++ src/Collection/ValidatorCollection.php
@@ -3,19 +3,22 @@ declare(strict_types = 1);
 
 namespace Budkovsky\Aid\Collection;
 
-use Budkovsky\Aid\Abstraction\CollectionAbstract;
+use Budkovsky\Aid\Abstraction\CollectionIndexedAbstract;
 use Budkovsky\Aid\Validator\Abstraction\ValidatorAbstract;
 
-class ValidatorCollection extends CollectionAbstract
+class ValidatorCollection extends CollectionIndexedAbstract
 {
 
-    public function add(?ValidatorAbstract $validator = null): ValidatorCollection
+    public function add(?string $index = null, ?ValidatorAbstract $validator = null): ValidatorCollection
     {
-        if ($validator) {
-            $this->collection[] = $validator;
+        if ($index && $validator) {
+            $this->collection[$index] = $validator;
         }
 
         return $this;
     }
+    public function get(string $index): ?ValidatorAbstract
+    {
+        return $this->collection[$index] ?? null;
+    }
 }
-
